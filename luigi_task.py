ff2a96f7fef7b98cea56a813f35ed1a07259c31f
Tiago Guimarães <tilacog@gmail.com>
2017-04-27 12:27:45 -0300

    work on insert task

--- luigi_task.py
+++ luigi_task.py
@@ -4,11 +4,12 @@ Dataflow to:
   2. download table data into files
   3. transform and insert downloaded tables into a PostgreSQL database
 """
-import luigi
 import json
 from pathlib import Path
 
-from sped_soap import download, fetch_all_table_data
+import luigi
+
+from sped_soap import download, fetch_all_table_data, make_database_record
 
 THIS_PATH = Path(__file__).parent.absolute()
 
@@ -52,7 +53,6 @@ class DownloadFiles(luigi.Task):
 
         return data
 
-
     def run(self):
         "downloads lots of files asynchronously"
 
@@ -82,20 +82,61 @@ class InsertIntoDatabase(luigi.Task):
     "Generate records from a single file and inserts them into a database"
 
     table_metadata_file = luigi.Parameter()
+    remapped_sped_names = {
+        'SpedFiscal': 'efd',
+        'SpedPisCofins': 'efd_contribuicoes',
+        'SpedContabil': 'ecd',
+        'SpedEcf': 'ecf',
+    }
 
     def requires(self):
         return DownloadFiles()
 
     def output(self):
-        pass  # TODO
+        "touched file (like a ticket) with database insertion result"
+        dest_dir = THIS_PATH / 'insertion_results'
+        dest_dir.mkdir(exist_ok=True)
+
+        input_file = Path(self.table_metadata_file)
+        basename = input_file.name.split('.', maxsplit=1)
+
+        ticket = 'db-insert-results-for-%s' % basename
+        return luigi.LocalTarget((dest_dir / ticket).as_posix())
+
+    def mark_ticket(self, result: str):
+        with self.output().open('w') as f:
+            f.write(result)
 
     def run(self):
-        pass  # TODO
+        with open(self.table_metadata_file) as fh:
+            metadata = json.load(fh)
 
-    def complete(self):
-        return True  # TODO
+        # find local table file
+        source_dir = Path(self.input().path)
+        basename = metadata.pop('basename')
+        table_file = source_dir / basename
 
-class UpdateDatabase(luigi.Task):  # TODO: Try to use it as a wrapper task
+        # build database record
+        sped_name = self.remapped_sped_names[metadata.pop('sped-name')]
+
+        try:
+            database_record = make_database_record(
+                sped_name=sped_name,
+                table_data=metadata,
+                filepath=table_file.as_posix()
+            )
+        except RuntimeError:  # table is empty
+            self.mark_ticket('EMPTY')
+            return
+
+        self.insert(database_record)
+
+    def insert(self, database_record: dict) -> bool:
+        # TODO
+        pass
+
+
+class UpdateDatabase(luigi.Task):
 
     def run(self):
 
@@ -105,6 +146,3 @@ class UpdateDatabase(luigi.Task):  # TODO: Try to use it as a wrapper task
         dependent_tasks = [InsertIntoDatabase(table_metadata_file=x.as_posix())
                            for x in table_data_dir.glob('*.json')]
         yield dependent_tasks
-
-
-
